87532757c6ca1d26f17b6448ed86bfa5f2121e2f
jatinpandey02 <jatinpandey02@gmail.com>
2021-07-09 18:38:16 +0530

    commit

--- Graph.js
+++ Graph.js
@@ -1,79 +1,3 @@
-function MinHeap() {
-    this.data = [];
-  }
-  
-  MinHeap.prototype.insert = function(val) {
-    this.data.push(val);
-    this.bubbleUp(this.data.length-1);
-  };
-  
-  MinHeap.prototype.bubbleUp = function(index) {
-    while (index > 0) {
-      // get the parent
-      var parent = Math.floor((index + 1) / 2) - 1;
-      
-      // if parent is greater than child
-      if (this.data[parent][1] > this.data[index][1]) {
-        // swap
-        var temp = this.data[parent];
-        this.data[parent] = this.data[index];
-        this.data[index] = temp;
-      }
-      
-      index = parent;
-    }
-  };
-  
-  MinHeap.prototype.extractMin = function() {
-    var min = this.data[0];
-    
-    // set first element to last element
-    if(this.data.length==1){
-        return this.data.pop();
-    }
-
-    this.data[0] = this.data.pop();
-
-    this.bubbleDown(0);
-    return min;
-  };
-  
-  MinHeap.prototype.bubbleDown = function(index) {
-    while (true) {
-      var child = (index+1)*2;
-      var sibling = child - 1;
-      var toSwap = null;
-      
-      // if current is greater than child
-      if(child>=this.data.length){
-          break;
-      }
-      if (this.data[index][1] > this.data[child][1]) {
-        toSwap = child;
-      }
-      
-      // if sibling is smaller than child, but also smaller than current
-      if (this.data[index][1] > this.data[sibling][1] && (this.data[child] == null || (this.data[child] !== null && this.data[sibling][1] < this.data[child][1]))) {
-          toSwap = sibling;
-      }
-      
-      // if we don't need to swap, then break.
-      if (toSwap == null) {
-        break;
-      }
-      
-      var temp = this.data[toSwap];
-      this.data[toSwap] = this.data[index];
-      this.data[index] = temp;
-      
-      index = toSwap;
-    }
-  };    
-  
-  MinHeap.prototype.isEmpty = function(){
-      return this.data.length==0
-  }
-
 class familyGraph{
     constructor(name){
         this.graph = new Map()
@@ -90,6 +14,10 @@ class familyGraph{
                 else if(x=="Sister"){
                     this.graph.get(name).push([i[0],"Father"])
                     this.graph.get(i[0]).push([name,"Daughter"])
+                }
+                else if(x=="Mother"){
+                    this.graph.get(name).push([i[0],"Husband"])
+                    this.graph.get(name).push([name,"Wife"])
                 }
             }
             else if(i[1]=="Mother"){
@@ -213,21 +141,22 @@ class familyGraph{
         return t;
     }
     shortestPath(from,to){
-        let pq = new MinHeap()
-        pq.insert([from,0])
+        let pq = []
+        pq.push([from,0])
         let dist = new Map()
         let parent = new Map()
         parent.set(from,-1)
         for(let i of this.graph)
             dist.set(i[0],1000)
         dist.set(from,0)
-        while(!pq.isEmpty()){
-            let u = pq.extractMin()
+        while(pq.length>0){
+            let u = pq[0]
+            pq.shift()
             for(let i of this.graph.get(u[0])){
                 if(dist.get(i[0])> dist.get(u[0])+1){
                     dist.set(i[0],dist.get(u[0]) + 1);
                     parent.set(i[0],u[0]);
-                    pq.insert([i[0],dist[i[0]]]);
+                    pq.push([i[0],dist[i[0]]]);
                 }
             }
         }
